b6513f4d19351b2b4fe020aa2ad71cf4ccede52c
GOKUL-A-N <gokulcricketer141103@gmail.com>
2023-11-26 22:32:07 +0530

    added front-end

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -14,7 +14,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-// app.use(cors({origin: true, credentials: true}));
+app.use(cors());
 
 app.get('/', (req, res) => { 
     res.send("Welcome Back Chief!");
